64903846a30196491ec7631071c3c58d0cc361a4
unknown <pablogv98@gmail.com>
2024-08-22 17:30:39 +0200

    new changes back

--- backend/pep_back/settings.py
+++ backend/pep_back/settings.py
@@ -38,14 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders',
     'rest_framework.authtoken',
-    'api',
     'corsheaders',
+    'api',
 ]
 
 MIDDLEWARE = [
-    'corshearders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
